0791812850c338768869bdbb95c82e66b750c231
henghe57 <seng_heng_hyi@students.edu.sg>
2025-09-27 07:31:18 +0000

    Co-authored-by: DLEE <autom8ton@users.noreply.github.com>

--- u14_functions/u14_functions.py
+++ u14_functions/u14_functions.py
@@ -7,7 +7,7 @@ hello()
 
 print("----------------")
 
-name = "Qis"
+name = "Qistina"
 def greeting(name):
   print(f"Hello, {name}")
 
@@ -49,14 +49,42 @@ greet_users(["Alice", "Bob", "Charlie"])
 print("---------------------------------------")
 
 # with 1 variable
-def calculator(x, y, z):
-  if z == "+":
-    total = x + y
-  elif z == "-":
-    total = x - y
-  elif z == "*":
-    total = x * y
-  elif z == "/":
-    total = x / y
+# def calculator(num1, num2, operator):
+def calculator(num1, num2, operator):
+  if operator == "+":
+    total = num1 + num2
+  elif operator == "-":
+    total = num1 - num2
+  elif operator == "*":
+    total = num1 * num2
+  elif operator == "/":
+    total = num1 / num2
   return total
 print(calculator(10,5,"/"))
+
+print("---------------------------------------")
+
+def is_palindrome(word):
+  if word == word[::-1]:
+    return True
+  else:
+    return False
+  
+print("Is 'radar' a palindrome? {}".format(is_palindrome("radar")))
+print("Is 'hello' a palindrome? {}".format(is_palindrome("hello")))
+
+print("---------------------------------------")
+
+def area_rectangle(length, breadth):
+  area = length * breadth 
+
+  return area
+
+area1 = area_rectangle(31,47)
+area2 = area_rectangle(56,77)
+
+print(f"The total area of area1 and area2 is {area1 + area2}")
+
+# line of execution
+# 71, 66, 67, 69, 71 # phase 1
+# 72, 66, 67, 69, 72 # phase 2
