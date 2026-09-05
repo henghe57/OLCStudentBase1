c13348f5220009779314908a47cb22e91943619d
henghe57 <seng_heng_hyi@students.edu.sg>
2026-09-05 07:35:02 +0000

    Co-authored-by: DLEE <autom8ton@users.noreply.github.com>

--- Debugging_practice/TempCv.py
+++ Debugging_practice/TempCv.py
@@ -62,14 +62,15 @@
 # [10]
 # Save your program.
 
+
 def displayWelcome():
     print("This program will convert a range of temperatures")
     print("Enter (F) to convert Fahrenheit to Celsius")
     print("Enter (C) to convert Celsius to Fahrenheit\n")
 
 def getConvertTo():
-    which = input("Enter selection: ") #1 missing ") at the end
-    while which != "F" and which != "C": #3 loop only when is non F or non C
+    which = input("Enter selection: ") #1 Missing ")
+    while which != "F" and which != "C": #3 Only loop non Fs or non Cs + #11 include both C and F not just either of them
         which = input("Enter selection: ") #2 indentation 
     return which
 
@@ -78,29 +79,31 @@ def displayFahrenToCelsius(start, end):
     print("Fahrenheit", "Celsius")
 
     for temp in range(start, end + 1):
-        converted_temp = (temp - 32) * 5/9 #4 subtract first before multiply
-        print("{:4.1f}      {:4.1f}".format(temp, converted_temp)) #5 should be converted_temp
+        converted_temp = (temp - 32) * 5/9 #5 need brackets to function formula properly
+        print("{:4.1f}      {:4.1f}".format(temp, converted_temp)) #4 2nd result shld be the end result 
 
 def displayCelsiusToFahren(start, end):
     print("\n Degrees", "Degrees")
     print(" Celsius", "Fahrenheit")
 
-    for temp in range(start, end + 1): #9 + 1 is needed 
-        converted_temp = (temp * 9/5) + 32 #11 incorrect formula 
+    for temp in range(start, end + 1): #12 +1 is needed to display the end result properly
+        converted_temp = (9/5 * temp) + 32 #10 incorrect formula 
         print("{:4.1f}      {:4.1f}".format(temp, converted_temp))
+
+# --- main
 
 #Display program welcome
 displayWelcome()
 
 # Get which conversion from user
-which = getConvertTo() #10 request for conversion 
+which = getConvertTo() #7 To get either F or C or error
 
 # Get range of temperatures to convert
 temp_start = int(input("Enter starting temperature to convert: "))
-temp_end = int(input("Enter ending temperature to convert: ")) #8 int() conversion needed 
+temp_end = int(input("Enter ending temperature to convert: ")) #9 temp is an integer
 
 # Display range of converted temperatures
 if which == "F":
-    displayFahrenToCelsius(temp_start, temp_end) #7 swapped positions
-elif which == "C": #6 should be in caps
-    displayCelsiusToFahren(temp_start, temp_end) #7 swapped positions
+    displayFahrenToCelsius(temp_start, temp_end) #8 swapped positions
+elif which == "C":#6 Capitalised C 
+    displayCelsiusToFahren(temp_start, temp_end) #8 swapped positions 
